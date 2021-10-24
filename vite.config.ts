2cd7e7f3f0b546addd9b641244a7207aa0afa0cf
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-10-24 13:18:27 -0600

    Try to fix compile-time type checks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tsconfigPaths from "vite-tsconfig-paths";
+// import eslintPlugin from "vite-plugin-eslint";
+// import checker from "vite-plugin-checker";
 
-// https://vitejs.dev/config/
+// console.log(checker);
+
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		tsconfigPaths({ projects: ["./tsconfig.prod.json"] }),
+		// eslintPlugin.default(),
+		// eslintPlugin.default({
+		// 	include: ["src/**/*.ts", "src/**/*.json", "src/**/*.d.ts", "src/**/*.vue"],
+		// }),
+		// checker.default({
+		// 	// this should yell at TransactionEdit.vue and toCurrency.ts, but doesn't
+		// 	typescript: {
+		// 		root: ".",
+		// 		tsconfigPath: "tsconfig.prod.json",
+		// 	},
+		// 	vueTsc: true,
+		// }),
+	],
 });
