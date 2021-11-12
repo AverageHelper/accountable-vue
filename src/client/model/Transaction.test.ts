0e5863cf7f8970a43ef546e5f0dd46a78d6ff3a5
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-11-12 16:15:07 -0700

    Fix our unit test lol

--- src/client/model/Transaction.test.ts
+++ src/client/model/Transaction.test.ts
@@ -3,16 +3,16 @@ import { Transaction } from "./Transaction";
 
 describe("Transaction record", () => {
 	test.each`
-		type         | amount
-		${"expense"} | ${-10}
-		${"expense"} | ${-1}
-		${"expense"} | ${-0.1}
-		${"expense"} | ${-0.01}
-		${"zero"}    | ${0}
-		${"income"}  | ${0.01}
-		${"income"}  | ${0.1}
-		${"income"}  | ${1}
-		${"income"}  | ${10}
+		type             | amount
+		${"expense"}     | ${-10}
+		${"expense"}     | ${-1}
+		${"expense"}     | ${-0.1}
+		${"expense"}     | ${-0.01}
+		${"transaction"} | ${0}
+		${"income"}      | ${0.01}
+		${"income"}      | ${0.1}
+		${"income"}      | ${1}
+		${"income"}      | ${10}
 	`(
 		"returns a type of '$type' if the record amount is $amount",
 		({ type, amount }: { type: TransactionRecordType; amount: number }) => {
