fbc0c5a92bfb3f41c25731c1b441527c464533c0
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-11-28 20:04:20 -0700

    Add links to our GitHub repository

--- src/client/components/AppVersion.vue
+++ src/client/components/AppVersion.vue
@@ -1,19 +1,43 @@
 <script setup lang="ts">
+import GitHubIcon from "../icons/GitHub.vue";
 import { computed } from "vue";
 import { version as v } from "../version";
 
 const version = computed(() => v);
+const repositoryUrl = computed(
+	() => `https://github.com/AverageHelper/accountable-vue/tree/v${version.value}`
+);
 </script>
 
 <template>
-	<p>Accountable v{{ version }}</p>
+	<div class="version">
+		<a :href="repositoryUrl" target="_blank"><GitHubIcon /></a>
+		<a :href="repositoryUrl" target="_blank">Accountable v{{ version }}</a>
+	</div>
 </template>
 
 <style scoped lang="scss">
 @use "styles/colors" as *;
 
-p {
+.version {
+	display: flex;
+	flex-flow: row nowrap;
 	color: color($secondary-label);
 	margin: 0;
+
+	.icon {
+		margin-right: 4pt;
+	}
+
+	a {
+		color: color($secondary-label);
+		text-decoration: none;
+
+		@media (hover: hover) {
+			&:hover {
+				text-decoration: underline;
+			}
+		}
+	}
 }
 </style>
