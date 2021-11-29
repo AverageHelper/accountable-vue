0585672966b60ed7e8dbc36c5652996cbafa8adf
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-11-28 20:23:16 -0700

    Update unit tests

--- src/client/model/Transaction.test.ts
+++ src/client/model/Transaction.test.ts
@@ -1,23 +1,37 @@
 import type { TransactionRecordType } from "./Transaction";
+import { dinero, toSnapshot } from "dinero.js";
+import { intlFormat } from "../filters/toCurrency";
 import { Transaction } from "./Transaction";
+import { USD } from "@dinero.js/currencies";
 
 describe("Transaction record", () => {
-	test.each`
-		type             | amount
-		${"expense"}     | ${-10}
-		${"expense"}     | ${-1}
-		${"expense"}     | ${-0.1}
-		${"expense"}     | ${-0.01}
-		${"transaction"} | ${0}
-		${"income"}      | ${0.01}
-		${"income"}      | ${0.1}
-		${"income"}      | ${1}
-		${"income"}      | ${10}
+	describe.each`
+		type             | amount   | formatted
+		${"expense"}     | ${-10}   | ${"($10.00)"}
+		${"expense"}     | ${-1}    | ${"($1.00)"}
+		${"expense"}     | ${-0.1}  | ${"($0.10)"}
+		${"expense"}     | ${-0.01} | ${"($0.01)"}
+		${"transaction"} | ${0}     | ${"$0.00"}
+		${"income"}      | ${0.01}  | ${"$0.01"}
+		${"income"}      | ${0.1}   | ${"$0.10"}
+		${"income"}      | ${1}     | ${"$1.00"}
+		${"income"}      | ${10}    | ${"$10.00"}
 	`(
-		"returns a type of '$type' if the record amount is $amount",
-		({ type, amount }: { type: TransactionRecordType; amount: number }) => {
-			const transaction = new Transaction("", "", { amount });
-			expect(transaction.type).toBe(type);
+		"amount $amount USD",
+		(params: { type: TransactionRecordType; amount: number; formatted: string }) => {
+			const { type, amount, formatted } = params;
+			const transaction = new Transaction("", "", {
+				// "amount" is number of smallest unit (cents). Multiply by 100 for USD.
+				amount: toSnapshot(dinero({ amount: amount * 100, currency: USD })),
+			});
+
+			test(`returns a type of '${type}'`, () => {
+				expect(transaction.type).toBe(type);
+			});
+
+			test(`currency format returns '${formatted}'`, () => {
+				expect(intlFormat(transaction.amount)).toBe(formatted);
+			});
 		}
 	);
 });
