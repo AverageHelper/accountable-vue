ddda22aa43484fdeee15123c6ec25aafa8fb132b
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-01-24 21:47:40 -0700

    Don't use purgecss until I figure it out

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -6,29 +6,29 @@
  */
 
 const autoprefixer = require("autoprefixer");
-const purgecss = require("@fullhuman/postcss-purgecss");
+// const purgecss = require("@fullhuman/postcss-purgecss");
 
-const IN_PRODUCTION = process.env.NODE_ENV !== "development";
+// const IN_PRODUCTION = process.env.NODE_ENV !== "development";
 
 module.exports = {
 	plugins: [
 		autoprefixer,
-		IN_PRODUCTION &&
-			// Purge unused CSS rules (lookin' at you, Bootstrap)
-			purgecss({
-				content: ["./**/*.html", "./src/**/*.vue"],
-				defaultExtractor(content) {
-					const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
-					return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
-				},
-				whitelist: [],
-				// Leave Vue stuff alone
-				whitelistPatterns: [
-					/-(leave|enter|appear)(|-(to|from|active))$/,
-					/^(?!(|.*?:)cursor-move).+-move$/,
-					/^router-link(|-exact)-active$/,
-					/data-v-.*/,
-				],
-			}),
+		// IN_PRODUCTION &&
+		// 	// Purge unused CSS rules (lookin' at you, Bootstrap)
+		// 	purgecss({
+		// 		content: ["./**/*.html", "./src/**/*.vue"],
+		// 		defaultExtractor(content) {
+		// 			const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
+		// 			return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+		// 		},
+		// 		whitelist: [],
+		// 		// Leave Vue stuff alone
+		// 		whitelistPatterns: [
+		// 			/-(leave|enter|appear)(|-(to|from|active))$/,
+		// 			/^(?!(|.*?:)cursor-move).+-move$/,
+		// 			/^router-link(|-exact)-active$/,
+		// 			/data-v-.*/,
+		// 		],
+		// 	}),
 	],
 };
