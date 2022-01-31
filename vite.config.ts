6fd3552e056275ebde3f7416fdf8bfbe5815b85b
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-01-31 11:47:25 -0700

    Add a module analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "vite";
+import analyze from "rollup-plugin-analyzer";
 import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
 	plugins: [
-		vue(), //
+		vue(),
 		tsconfigPaths({ projects: ["./tsconfig.prod.json"] }),
+		analyze({
+			filter: module => {
+				// Decide which modules are important enough to warn about:
+				return (
+					// Only modules that themselves take >8% of the bundle
+					module.percent > 8 &&
+					// Not Vue (that's bound to be big no matter what)
+					module.id !== "/node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js"
+				);
+			},
+		}),
 	],
 	resolve: {
 		alias: {
