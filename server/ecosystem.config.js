1b931fcddcb1dcff0b821c94f82b94c82c6e7ea3
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-02-16 12:14:15 -0700

    Add an ecosystem file for PM2

---
+++ server/ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	apps: [
+		{
+			name: "accountable",
+			script: "./dist/main.js",
+			cwd: __dirname,
+			source_map_support: true,
+			watch: ["dist"],
+			watch_delay: 1000,
+			time: true,
+			env: {
+				NODE_ENV: "development",
+			},
+			env_production: {
+				NODE_ENV: "production",
+			},
+		},
+	],
+};
