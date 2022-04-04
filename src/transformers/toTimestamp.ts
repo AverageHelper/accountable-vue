4d24d58cc5449ecc302d87afde5e830b237cf253
James Robinson <1500092+AverageHelper@users.noreply.github.com>
2022-04-04 16:36:01 -0600

    Do 24-hr time (#24)
    * Do 24-hr time
    
    * v0.5.6
    
    * Fix a backend vulnerability

--- src/transformers/toTimestamp.ts
+++ src/transformers/toTimestamp.ts
@@ -1,4 +1,16 @@
 export function toTimestamp(date: Date): string {
-	const formatter = new Intl.DateTimeFormat(undefined, { dateStyle: "medium", timeStyle: "short" });
+	const formatter = new Intl.DateTimeFormat(undefined, {
+		dateStyle: "medium",
+		timeStyle: "short",
+		hour12: isLocale12Hr(),
+	});
 	return formatter.format(date);
+}
+
+export function isLocale12Hr(): boolean {
+	// Initial idea from https://stackoverflow.com/a/60437579.
+	// Here, we default to 24-hr time because it's better, and
+	// `formatToParts` doesn't detect that I've set 24-hr time
+	// in a 12-hr locale.
+	return new Intl.DateTimeFormat().resolvedOptions().hour12 ?? false;
 }
