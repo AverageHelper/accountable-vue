ebd5b5611ed9fb4703ca8c0534706e110652d667
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-06-09 15:51:28 -0600

    More i18n

--- src/About.vue
+++ src/About.vue
@@ -28,38 +28,61 @@ import OutLink from "./components/OutLink.vue";
 			</template>
 		</i18n-t>
 		<ul>
-			<li>You may have heard of <OutLink to="https://www.youneedabudget.com">YNAB</OutLink>.</li>
-			<li>
-				<OutLink to="https://www.intuit.com">Intuit</OutLink> makes
-				<OutLink to="https://mint.intuit.com">Mint</OutLink>. These guys also make
-				<OutLink to="https://turbotax.intuit.com">TurboTax</OutLink> and other fintech products you
-				might have heard about.</li
-			>
-			<li>
-				<OutLink to="https://www.ramseysolutions.com/">Dave Ramsey</OutLink> makes
-				<OutLink to="https://www.ramseysolutions.com/ramseyplus/everydollar">EveryDollar</OutLink>.
-				I subscribe to many of Ramsey's financial principles. Stay out of debt, folks.</li
-			>
-			<li
-				>There are
-				<OutLink to="https://www.nerdwallet.com/article/finance/best-budget-apps"
-					>lots of others</OutLink
-				>, but I've only tried these. Check them out if you're curious! I'm sure they're great.</li
-			>
+			<i18n-t keypath="about.alternatives.alt1" tag="li">
+				<template #ynab>
+					<OutLink to="https://www.youneedabudget.com">{{ $t("about.alternatives.ynab") }}</OutLink>
+				</template>
+			</i18n-t>
+
+			<i18n-t keypath="about.alternatives.alt2" tag="li">
+				<template #intuit>
+					<OutLink to="https://www.intuit.com">{{ $t("about.alternatives.intuit") }}</OutLink>
+				</template>
+				<template #mint>
+					<OutLink to="https://mint.intuit.com">{{ $t("about.alternatives.mint") }}</OutLink>
+				</template>
+				<template #turbotax>
+					<OutLink to="https://turbotax.intuit.com">{{
+						$t("about.alternatives.turbotax")
+					}}</OutLink>
+				</template>
+			</i18n-t>
+
+			<i18n-t keypath="about.alternatives.alt3" tag="li">
+				<template #ramsey>
+					<OutLink to="https://www.ramseysolutions.com/">{{
+						$t("about.alternatives.ramsey")
+					}}</OutLink>
+				</template>
+				<template #everydollar>
+					<OutLink to="https://www.ramseysolutions.com/ramseyplus/everydollar">{{
+						$t("about.alternatives.everydollar")
+					}}</OutLink>
+				</template>
+			</i18n-t>
+
+			<i18n-t keypath="about.alternatives.alt4" tag="li">
+				<template #others>
+					<OutLink to="https://www.nerdwallet.com/article/finance/best-budget-apps">{{
+						$t("about.alternatives.others")
+					}}</OutLink>
+				</template>
+			</i18n-t>
 		</ul>
-		<p
-			>I'm sure these are brilliant, but none of them are
-			<OutLink to="https://en.wikipedia.org/wiki/Open-source_software">open-source</OutLink>. All of
-			these keep your data for you, and hang onto the encryption keys to that data; they're not
-			<OutLink to="https://en.wikipedia.org/wiki/End-to-end_encryption"
-				>end-to-end encrypted</OutLink
-			>. I don't know what all they do with your data, and I don't trust them to tell you.</p
-		>
-		<p
-			>Part of their business model is to be the gateway to your financial records. If you just want
-			help with your budget, and want someone else to keep your pocketbook for you, then any of the
-			above may suit you just fine.</p
-		>
+
+		<i18n-t keypath="about.alternatives.open-source" tag="p">
+			<template #opensource>
+				<OutLink to="https://en.wikipedia.org/wiki/Open-source_software">{{
+					$t("about.alternatives.opensource")
+				}}</OutLink>
+			</template>
+			<template #e2ee>
+				<OutLink to="https://en.wikipedia.org/wiki/End-to-end_encryption">{{
+					$t("about.alternatives.e2ee")
+				}}</OutLink>
+			</template>
+		</i18n-t>
+		<i18n-t keypath="about.alternatives.summary" tag="p" />
 		<!-- TODO: Reorganize. Mention https://www.budgetwithbuckets.com/ -->
 
 		<Footer />
