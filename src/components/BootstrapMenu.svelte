6c445fc41800ff2e38c8265e238a377d189488a5
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-07-23 09:47:04 -0600

    Return BootstrapMenu functionality

--- src/components/BootstrapMenu.svelte
+++ src/components/BootstrapMenu.svelte
@@ -2,7 +2,7 @@
 	import { _ } from "svelte-i18n";
 	import { aboutPath, homePath, installPath, loginPath, securityPath } from "../router";
 	import { isLoginEnabled } from "../store";
-	import { useLocation } from "svelte-navigator";
+	import { link, useLocation } from "svelte-navigator";
 
 	interface Page {
 		path: string;
@@ -36,10 +36,14 @@
 	}
 </script>
 
-<nav class="navbar navbar-expand-sm navbar-dark">
-	<!-- TODO: I18N -->
-	<a href={homeRoute} class="navbar-brand" role="heading" aria-label="Accountable"
-		>A&cent;countable</a
+<nav class="navbar-147f68de navbar navbar-expand-sm navbar-dark">
+	<a
+		href={homeRoute}
+		class="navbar-brand-9794720e"
+		role="heading"
+		aria-label={$_("common.accountable")}
+		title={$_("common.accountable")}
+		use:link>A&cent;countable</a
 	>
 	<button
 		class="navbar-toggler {!isNavButtonOpen ? 'collapsed' : ''}"
@@ -57,8 +61,8 @@
 	<div id="navbarNav" class="collapse navbar-collapse {isNavButtonOpen ? 'show' : ''}">
 		<ul class="navbar-nav mr-auto">
 			<!-- <li class="nav-item">
-				<span class="visually-hidden">Current Language: {currentLocale.language}</span>
-				<p class="flag-icon">{currentLocale.flag}</p>
+				<span class="visually-hidden">Current Language: {$locale}</span>
+				<p class="flag-icon">{$locale}</p>
 			</li> -->
 			{#each pages as page (page.path)}
 				<li class="nav-item {currentPath === page.path ? 'active' : ''}">
@@ -66,6 +70,7 @@
 						class="nav-link {currentPath === page.path ? 'active' : ''}"
 						href={page.path}
 						on:click={close}
+						use:link
 						>{$_(page.titleKey)}
 						{#if currentPath === page.path}
 							<span class="visually-hidden">{$_("common.current-aside")}</span>
@@ -80,55 +85,81 @@
 <style type="text/scss">
 	@use "styles/setup" as *;
 	@use "styles/colors" as *;
-	@import "styles/bootstrap"; // Let's only use this here, if we can
 
-	.navbar-brand {
+	// Import only the Bootstrap we need:
+	@import "bootstrap/scss/functions";
+	@import "bootstrap/scss/variables";
+	@import "bootstrap/scss/mixins";
+	@import "bootstrap/scss/reboot";
+	@import "bootstrap/scss/transitions";
+	@import "bootstrap/scss/nav";
+	@import "bootstrap/scss/navbar";
+	@import "bootstrap/scss/helpers/_visually-hidden.scss";
+
+	$change: 575px;
+
+	.navbar-brand-9794720e {
 		display: block;
-		transform: translateX(-50%);
-		position: absolute;
-		left: 50%;
 		font-weight: bold;
 		font-size: x-large;
-	}
+		z-index: 50;
+		margin-left: 16pt;
+		text-decoration: none;
+		color: color($label);
 
-	@include mq($until: desktop) {
-		.navbar-brand {
-			width: 100%;
-			text-align: left;
-			margin-left: 16pt;
+		&:hover {
+			color: color($link);
+			text-decoration: underline;
 		}
 	}
 
-	ul,
-	.navbar-toggler {
-		list-style-type: none;
-		position: absolute;
-		right: 0;
-		margin: 0;
-		margin-right: 8pt;
-	}
+	nav.navbar.navbar-147f68de {
+		display: flex;
+		flex-flow: row nowrap;
+		width: 100%;
 
-	@include mq($until: 575px) {
-		.navbar {
-			position: relative;
-
-			&-nav {
-				z-index: 100;
-				margin-right: 0;
-				margin-top: 22pt;
-				padding: 0 8pt;
-				text-align: right;
+		#navbarNav {
+			@include mq($until: $change) {
+				position: absolute;
+				top: 1.8em;
+				right: 0;
 				width: 100%;
-				background-color: color($navbar-background);
 			}
 		}
-	}
 
-	.active {
-		font-weight: bold;
-	}
+		ul,
+		.navbar-toggler {
+			list-style-type: none;
+			margin-left: auto;
+			margin-right: 8pt;
+			z-index: 50;
+		}
 
-	nav.navbar {
+		@include mq($until: $change) {
+			.navbar {
+				position: relative;
+
+				&-nav {
+					z-index: 100;
+					margin-right: 0;
+					margin-top: 22pt;
+					padding: 0 8pt;
+					text-align: right;
+					width: 100%;
+					background-color: color($navbar-background);
+				}
+			}
+		}
+
+		.active {
+			font-weight: bold;
+		}
+
+		.flag-icon {
+			margin: 0;
+			padding: 0;
+		}
+
 		.nav-item {
 			&.active {
 				color: color($link);
@@ -140,10 +171,5 @@
 				}
 			}
 		}
-	}
-
-	.flag-icon {
-		margin: 0;
-		padding: 0;
 	}
 </style>
