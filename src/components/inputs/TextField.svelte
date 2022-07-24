d3469a4ba019b1a3a5ace22f89e2f8abc89b9224
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-07-24 09:49:58 -0600

    Fix text field "required" indicator

--- src/components/inputs/TextField.svelte
+++ src/components/inputs/TextField.svelte
@@ -94,7 +94,7 @@
 	<div class="text-input-c8d2e7c2__label" on:click={focus}>
 		{label}
 		{#if required && showsRequired}
-			<span class="text-input-c8d2e7c2__required">{$_("text-input-c8d2e7c2.required")}</span>
+			<span class="text-input-c8d2e7c2__required">{$_("text-input.required")}</span>
 		{/if}
 	</div>
 	{#if disabled}
